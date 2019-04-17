be760f34f2fe1ff2810777ff8779d25f11319367
sptkl <caobaiyue@gmail.com>
2019-04-17 14:38:15 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # api-geosupport
 flask api for geosupport python bindings
+
+## Instructions
+1. Using Docker: 
+    ```
+    docker run -it\
+        -p 5000:5000\
+        --name geo\
+        -v `pwd`:/home/api-geosupport\
+            -w /home/api-geosupport\
+            sptkl/docker-geosupport:19a python app.py
+    ```
+2. Examples: 
+    + using borough: 
+        ```
+        http://0.0.0.0:5000/1b?house_number=120&street_name=broadway&borough=MN
+        ```
+    + using zipcode: 
+        ```
+        http://0.0.0.0:5000/1b?house_number=120&street_name=broadway&zipcode=10271
+        ```
+3. Notes: 
+    + Only address related functions are built out at this momment
+    + switch out `1b` for functions including `1`, `1a`, `1b` should all work
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request 
+from geosupport import Geosupport, GeosupportError
+
+g = Geosupport()
+app = Flask(__name__)
+
+@app.route('/<function>/<house_number>/<street_name>/<borough>/<zipcode>', methods=['GET'])
+@app.route('/<function>/')
+def geocode(function):
+    house_number = request.args.get('house_number', '')
+    street_name = request.args.get('street_name', '')
+    borough = request.args.get('borough', '')
+    zipcode = request.args.get('zipcode', '')
+    try: 
+        geo = g[function](house_number=house_number, 
+                        street_name=street_name, 
+                        borough=borough,
+                        zip_code=zipcode)
+
+        return jsonify({'status':'success', 
+                        'inputs': {
+                            'function': function,
+                            'house_number': house_number, 
+                            'street_name': street_name, 
+                            'borough': borough,
+                            'zipcode': zipcode
+                        },
+                        'results': geo})
+
+    except GeosupportError as e: 
+
+        return jsonify({'status': 'failure',
+                        'inputs': {
+                            'function': function,
+                            'house_number': house_number, 
+                            'street_name': street_name, 
+                            'borough': borough, 
+                            'zipcode': zipcode
+                        }, 
+                        'results': {
+                            'GRC': e.result['Geosupport Return Code (GRC)'], 
+                            'GRC2': e.result['Geosupport Return Code 2 (GRC 2)'],
+                            'message1': e.result['Message'],
+                            'message2': e.result['Message 2']
+                            }
+                        })
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
