0a2ff07332595cb3e50744661dbbb546590ae03b
sptkl <caobaiyue@gmail.com>
2019-04-29 11:53:47 -0400

    change port

--- app.py
+++ app.py
@@ -49,5 +49,4 @@ def geocode(function):
                         })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
